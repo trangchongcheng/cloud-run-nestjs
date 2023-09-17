1369980fbe1a2167657a0eed5f2fe3d98a2e3eab
Chien <cheng.cit@gmail.com>
2023-09-17 10:59:34 +0700

    update port cloud run

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    return { status: 200, message: 'Successfully' };
+    return { status: 200, message: 'Success 200' };
   }
 }
